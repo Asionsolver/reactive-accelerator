60473ab0c686da3652a48dc405b9511290c59f33
asionsolver <asionsolver274@gmail.com>
2024-01-10 01:17:38 +0600

    Learn new topic name like Javascript in JSX with curly Braces

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -6,8 +6,34 @@ const person = {
   }
 };
 
-export default function Information() {
+const styleOne = {
+  backgroundColor: 'black',
+  color: '#fff',
+  padding: '1.5rem',
+  listStyle: 'none',
+  fontWeight: 'semibold'
+};
 
+const styleTwo = {
+  backgroundColor: 'pink',
+  color: '#fff',
+  padding: '1.5rem',
+  listStyle: 'none',
+  fontWeight: 'semibold'
+};
+
+
+export default function Information() {
+  
+  
+  const color = 'pink';
+  // ! another way to do it. This is called a logical/Controller operator. This is the best practices.
+  let style = '';
+  if (color === 'pink'){
+    style = styleTwo;
+  } else {
+    style = styleOne;
+  }
   return (
     <div style={person.theme}>
       <h1>{person.name}s Todos</h1>
@@ -16,7 +42,8 @@ export default function Information() {
         src="https://i.imgur.com/7vQD0fPs.jpg"
         alt="Gregorio Y. Zara"
       />
-      <ul>
+      {/* <ul style={color === 'pink' ? styleTwo : styleOne}> */}
+      <ul style={style}>
         <li>Improve the videophone</li>
         <li>Prepare aeronautics lectures</li>
         <li>Work on the alcohol-fuelled engine</li>
