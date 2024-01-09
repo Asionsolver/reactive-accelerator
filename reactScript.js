a09532ff0cb4c52ed7cb53d3b3bbed1a7e984aa5
asionsolver <asionsolver274@gmail.com>
2024-01-09 14:24:40 +0600

    add to new file name like react.html and reactScript.js file

---
+++ reactScript.js
@@ -0,0 +1,62 @@
+// Simple js way to create an html element
+// const p = document.createElement('p');
+// p.textContent = 'This is a paragraph';
+// document.getElementById("root").appendChild(p);
+
+// React way to create an html element
+// const reactElement = React.createElement('div', null, React.createElement('h1', null, 'Hello World!'));
+
+// ReactDOM.createRoot(document.getElementById('root')).render(reactElement);
+
+// JSX way to create an html element
+const productPrice = 5000;
+
+function Product() {
+  const [price, setPrice] = React.useState(0);
+
+  function addToCart() {
+    setPrice(price + 1);
+  }
+  return (
+    <div className="shadow-md rounded-lg bg-slate-800 border-slate-700">
+      <a href="#">
+        <img
+          className="rounded-t-lg"
+          src="https://dummyimage.com/680X400/087ea4/ffffff.png&text=Product"
+          alt="product image"
+        />
+      </a>
+      <div className="px-5 py-5">
+        <div className="flex items-center justify-between mt-5">
+          <a href="#">
+            <h3 className="font-semibold text-xl tracking-tight">
+              Reactive Accelerator Course
+            </h3>
+          </a>
+          <span className="text-xl font-medium text-teal-500">
+            {price * productPrice}
+          </span>
+        </div>
+        <div className="flex items-center justify-between mt-5">
+          <span className="text-2xl font-bold text-slate-300">
+            {productPrice}
+          </span>
+          <a
+            onClick={addToCart}
+            href="#"
+            className="text-white focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-[#087ea4] hover:bg-[#087ea4]/[.8] focus:ring-[#087ea4]/[.5]"
+          >
+            Add to cart
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <>
+    <Product />
+    <Product />
+  </>
+);
