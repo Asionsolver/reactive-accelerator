27751895c6bc84a69900de9e007a3fe28102587c
asionsolver <asionsolver274@gmail.com>
2024-01-09 22:29:10 +0600

    Learn react importing and exporting components

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Profile as Card } from "./components/Gallery"
+import Gallery from "./components/Gallery"
+
+
+function App() {
+  return (
+    <>
+      <div>
+      <h1>Hello World!</h1>
+      <Gallery />
+      <Card />
+      </div>
+      
+    </>
+  )
+}
+
+export default App
